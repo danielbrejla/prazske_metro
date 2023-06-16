e2a961e22694c306376b3dacbf791e72060246e2
daniel.brejla <daniel.brejla@VT1-06>
2023-06-16 11:14:30 +0200

    Přidejte soubory projektu.

---
+++ prazske_metro/LinkaMetra.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prazske_metro
+{
+    internal class LinkaMetra
+    {
+        public string Nazevlinky { get; set; }
+        public string[] stanice { get; set; }
+        public bool aktivni { get; set; }
+        public string barva { get; set; }
+
+        public void vypisVsechnyStanice()
+        {
+            if (stanice.Length > 0)
+            {
+                foreach (var jednaStanice in stanice)
+                {
+                    Console.WriteLine(jednaStanice);
+                }
+            }
+        }
+        public void VypisPrvniPosledni()
+        {
+            if (stanice.Length >= 2)
+            {
+                Console.WriteLine(stanice[0]);
+                Console.WriteLine(stanice[stanice.Length - 1]);
+            }
+        }
+        public void Nastavlinku()
+        {
+            if (barva == "zelená")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (barva == "červená")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (barva == "žlutá")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (barva == "modrá")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine($"VÍTEJ V LINCE METRA {Nazevlinky}");
+
+        } 
+    }
+     
+    
+}
---
+++ prazske_metro/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using prazske_metro;
+
+string[] poleLinkaA = { "nemocnice motol","petřiny","nadr. veleslavin","bořislavka","dejvická","hradčanská","malostranská","můstek","muzeum","nam. míru","jiřího z poděbrad","flora","želivského","strašnická","skalka","depo hostivař" };
+string[] poleLinkaB = { "zlučin", "stodůlky", "luka", "lužiny", "hůrka", "nové butovice", "jinovice", "radlická", "smíchovské nadr.", "anděl", "karlovo nam.", "národní třída", "můstek", "nam. republiky", "florenc", "křižikova", "invalidovna", "palmovka", "českomoravská", "vysočanská", "kolbenova", "hloubětín", "rajská zahrada", "černý most" };
+string[] poleLinkaC = { "letnany", "prosek", "střížkov", "ládví", "kobylisy", "nadr. holešovice", "vltavská", "florenc", "hlavní nadraží", "muzeum", "i.p pavlova", "vyšehrad", "pražského povstání", "pankrác", "budějovická", "kačerov", "roztyly", "chodov", "opatov", "háje" };
+string[] poleLinkaD = { "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč", "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru" };
+Console.WriteLine("zadej linku kterou chceš využít");
+string volbaLinky = Console.ReadLine();
+if (volbaLinky == "A")
+{
+    LinkaMetra linkaA = new LinkaMetra() { Nazevlinky = "A", barva = "zelená", aktivni = true, stanice = poleLinkaA };
+    linkaA.Nastavlinku();
+    Console.WriteLine("chcete vypsat všechny stanice nebo první a poslední ");
+    string Volba = Console.ReadLine();
+    if(Volba== "1")
+    {
+        linkaA.vypisVsechnyStanice();
+    }
+    else if (Volba== "2")
+    {
+        linkaA.VypisPrvniPosledni();
+    }
+}
+if (volbaLinky == "B")
+{
+    LinkaMetra linkaB = new LinkaMetra() { Nazevlinky = "B", barva = "žlutá", aktivni = true, stanice = poleLinkaB };
+    linkaB.Nastavlinku();
+    Console.WriteLine("chcete vypsat všechny stanice nebo první a poslední ");
+    string Volba = Console.ReadLine();
+    if (Volba == "1")
+    {
+        linkaB.vypisVsechnyStanice();
+    }
+    else if (Volba == "2")
+    {
+        linkaB.VypisPrvniPosledni();
+    }
+}
+if (volbaLinky == "C")
+{
+    LinkaMetra linkaC = new LinkaMetra() { Nazevlinky = "C", barva = "červená", aktivni = true, stanice = poleLinkaC };
+    linkaC.Nastavlinku();
+    Console.WriteLine("chcete vypsat všechny stanice nebo první a poslední ");
+    string Volba = Console.ReadLine();
+    if (Volba == "1")
+    {
+        linkaC.vypisVsechnyStanice();
+    }
+    else if (Volba == "2")
+    {
+        linkaC.VypisPrvniPosledni();
+    }
+}
+if (volbaLinky == "D")
+{
+    LinkaMetra linkaD = new LinkaMetra() { Nazevlinky = "D", barva = "modrá", aktivni = true, stanice = poleLinkaD };
+    linkaD.Nastavlinku();
+    Console.WriteLine("chcete vypsat všechny stanice nebo první a poslední ");
+    string Volba = Console.ReadLine();
+    if (Volba == "1")
+    {
+        linkaD.vypisVsechnyStanice();
+    }
+    else if (Volba == "2")
+    {
+        linkaD.VypisPrvniPosledni();
+    }
+}
